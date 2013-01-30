07a786b7c7c15a6bfe6caeda4c57fc69bdfa8ec1
Adrian Sandy <adrianknowsbest@gmail.com>
2013-01-30 18:57:07 -0500

    finished input

--- mta.rb
+++ mta.rb
@@ -2,76 +2,80 @@
 require 'pry'
 require 'rainbow'
 
-puts "What Subway Line do you want? "
-
-puts "Do you want n, l OR 6 "
-trainresponse = gets.chomp
-
-
-if trainresponse == 'n'
-puts "where will you start on the N? "
-n = gets.chomp
-  elsif trainresponse =='l'
-puts "Where will you start on the L? "
-  elsif trainresponse =='6'
-l = gets.chomp
-puts "Where will you start on the 6? "
-six = gets.chomp
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 s = { "six" => [43,33,28,23,"gc",14],
                   "n" => [76,34,28,23,"gc",8],
                   "l" => [1,3,"gc",6,8]
                   }
+
+puts "What Subway Line do you want? "
+
+puts "Do you want n, l OR six "
+ont = gets.chomp
+
+print "What stop are you getting on? #{s[ont].join(" ")}"
+ons = gets.chomp
+
+print "What trian are you getting off? \n"
+ot = gets.chomp
+
+print "What stop are you getting on? #{s[ot].join(" ")}"
+os = gets.chomp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def distance(subway,ontrain,offtrain,onstop,offstop)
   if (ontrain==offtrain)
