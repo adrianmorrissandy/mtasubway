60659617563c9a64e2e4eb9dc234dd6afe00de74
Adrian Sandy <adrianknowsbest@gmail.com>
2013-01-30 18:28:04 -0500

    Please enter the commit message for your changes. Lines starting

---
+++ mta.rb
@@ -0,0 +1,19 @@
+require 'pry'
+require 'rainbow'
+
+puts "What Subway Line do you want? "
+
+puts "Do you want n, l OR 6 "
+trainresponse = gets.chomp
+
+
+if trainresponse == 'n'
+puts "where will you start on the N? "
+n = gets.chomp
+  elsif trainresponse =='l'
+puts "Where will you start on the L? "
+  elsif trainresponse =='6'
+l = gets.chomp
+puts "Where will you start on the 6? "
+six = gets.chomp
+end
