5d2ab3fe61197e18f309521bf85ec26e01bdace0
Blanchet <h55nick@gmail.com>
2013-01-30 19:07:34 -0500

    ending changes

--- mta.rb
+++ mta.rb
@@ -2,81 +2,6 @@
 require 'pry'
 require 'rainbow'
 
-
-
-s = { "six" => [43,33,28,23,"gc",14],
-                  "n" => [76,34,28,23,"gc",8],
-                  "l" => [1,3,"gc",6,8]
-                  }
-
-puts "What Subway Line do you want? "
-
-puts "Do you want n, l OR six "
-ont = gets.chomp
-
-print "What stop are you getting on? #{s[ont].join(" ")}"
-ons = gets.chomp
-
-print "What trian are you getting off? \n"
-ot = gets.chomp
-
-print "What stop are you getting on? #{s[ot].join(" ")}"
-os = gets.chomp
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def distance(subway,ontrain,offtrain,onstop,offstop)
   if (ontrain==offtrain)
     #same train make sure different stop and calc index difference
@@ -86,8 +11,34 @@ def distance(subway,ontrain,offtrain,onstop,offstop)
     #different train
     dis = (subway[ontrain].index(onstop)-subway[ontrain].index("gc")).abs + (subway[offtrain].index(offstop)-subway[offtrain].index("gc")).abs
   end
-print dis.to_s + " <<< Distance!"
+dis
 end
 
-print distance(s,"six","l",33,1)
 
+s = { "six" => ["43","33","28","23","gc","14"],
+                  "n" => ["76","34","28","23","gc","8"],
+                  "l" => ["1","3","gc","6","8"]
+                  }
+
+print `clear`
+
+puts "What Subway Line do you want? "
+
+print "Do you want n, l OR six\n--> "
+ont = gets.chomp
+
+print "What stop are you getting on? #{s[ont].join(" ")} \n--> "
+ons = gets.chomp
+
+print "What trian are you getting off? (n, l OR six) \n --> "
+ot = gets.chomp
+
+print "What stop are you getting on? #{s[ot].join(" ")}\n--> "
+os = gets.chomp
+
+
+
+print "You are" + " #{distance(s,ont,ot,ons,os)} ".foreground(:red) + "stop(s) away!"
+print "\n\n"
+gets
+print `clear`
